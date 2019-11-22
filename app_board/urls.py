46bc485f526c4dd36c659188cabc9563412a8c0d
ParkDoEun <pde140300@gmail.com>
2019-11-22 21:21:06 +0900

    first commit

--- app_board/urls.py
+++ app_board/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-import app_board.views
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',app_board.views.home, name="home"),
-    path('app_board/',include('app_board.urls')),
+    path('<int:post_id>/', views.detail, name="detail"),
+    path('new/', views.new, name="new"),
+    path('create/', views.create, name="create"),
+    path('edit/<int:post_id>', views.edit, name="edit"),
+    path('update/<int:post_id>', views.update, name="update"),
+    path('delete/<int:post_id>',views.delete, name="delete"),
 ]
--- app_board/views.py
+++ app_board/views.py
@@ -1,11 +1,44 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 def home(request) :
-    blogs = Post.objects
-    return render(request, 'home.html', {'blogs':blogs})
+    posts = Post.objects
+    return render(request, 'home.html', {'posts':posts})
 
 def detail(request, blog_id) :
-    blog_detail = get_object_or_404(Post, pk=blog_id)
-    return render(request, 'detail.html', {'blog':blog_detail})
+    post_detail = get_object_or_404(Post, pk=blog_id)
+    return render(request, 'detail.html', {'post':post_detail})
+
+def new(request) :
+    return render(request, 'new.html')
+
+def create(request) :
+    new_blog = Post()
+    new_blog.title = request.POST['title']
+    new_blog.writer = request.POST['writer']
+    new_blog.date = timezone.datetime.now()
+    new_blog.body = request.POST['body']
+    new_blog.save()
+    return redirect('/app_board/'+str(new_blog.id))
+
+def edit(request,post_id): 
+    edit_post = Post.objects.get(id = post_id)
+    return render(request, 'edit.html', {'post':edit_post})
+
+def update(request,post_id):
+    update_post = Post.objects.get(id = post_id)
+    update_blog.title = request.POST['title']
+    update_blog.writer = request.POST['writer']
+    new_blog.date = timezone.datetime.now()
+    update_blog.body=request.POST['body']
+    update_blog.save()
+    return redirect('/app_post'+str(update_blog.id))
+
+def delete(request, post_id):
+    delete_blog = Blog.objects.get( id = post_id)
+    delete_blog.delete()
+    return redirect('home')
+
+
