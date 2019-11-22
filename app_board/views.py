c2e39fafdc721294c5bcf977755aaa45b38cb06e
ParkDoEun <pde140300@gmail.com>
2019-11-22 21:53:48 +0900

    second

--- app_board/views.py
+++ app_board/views.py
@@ -7,21 +7,21 @@ def home(request) :
     posts = Post.objects
     return render(request, 'home.html', {'posts':posts})
 
-def detail(request, blog_id) :
-    post_detail = get_object_or_404(Post, pk=blog_id)
+def detail(request, post_id) :
+    post_detail = get_object_or_404(Post, pk=post_id)
     return render(request, 'detail.html', {'post':post_detail})
 
 def new(request) :
     return render(request, 'new.html')
 
 def create(request) :
-    new_blog = Post()
-    new_blog.title = request.POST['title']
-    new_blog.writer = request.POST['writer']
-    new_blog.date = timezone.datetime.now()
-    new_blog.body = request.POST['body']
-    new_blog.save()
-    return redirect('/app_board/'+str(new_blog.id))
+    new_post = Post()
+    new_post.name = request.POST['name']
+    new_post.writer = request.POST['writer']
+    new_post.date = timezone.datetime.now()
+    new_post.body = request.POST['body']
+    new_post.save()
+    return redirect('/app_board/'+str(new_post.id))
 
 def edit(request,post_id): 
     edit_post = Post.objects.get(id = post_id)
@@ -29,16 +29,16 @@ def edit(request,post_id):
 
 def update(request,post_id):
     update_post = Post.objects.get(id = post_id)
-    update_blog.title = request.POST['title']
-    update_blog.writer = request.POST['writer']
-    new_blog.date = timezone.datetime.now()
-    update_blog.body=request.POST['body']
-    update_blog.save()
-    return redirect('/app_post'+str(update_blog.id))
+    update_post.name = request.POST['name']
+    update_post.writer = request.POST['writer']
+    update_post.date = timezone.datetime.now()
+    update_post.body=request.POST['body']
+    update_post.save()
+    return redirect('/app_board/'+str(update_post.id))
 
 def delete(request, post_id):
-    delete_blog = Blog.objects.get( id = post_id)
-    delete_blog.delete()
+    delete_post = Post.objects.get( id = post_id)
+    delete_post.delete()
     return redirect('home')
 
 
